0a8b8cd534a6283762a25e4212cf465e5cebc5c2
YURIY NOVIKOV <YNIKONOV@Mac-mini-YURI.local>
2013-10-24 12:43:50 +0400

    Initial Commit

---
+++ HomeworkLesson1/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HomeworkLesson1
+//
+//  Created by YURIY NOVIKOV on 24.10.13.
+//  Copyright (c) 2013 YURIY NOVIKOV. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
